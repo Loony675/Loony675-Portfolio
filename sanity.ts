92b8abbe7b67ca221d270e000a323a4b5a17293f
Loussot <theo.loussot@gmail.com>
2023-01-25 17:10:43 +0100

    env hidden

--- sanity.ts
+++ sanity.ts
@@ -5,7 +5,7 @@ export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
     apiVersion:'2021-10-21',
-    useCdn: process.env.NODE_END === "production",
+    useCdn: process.env.NODE_ENV === "production",
 };
 
 // Set up the client for fetching data in the getProps page functions
