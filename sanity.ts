30138e153679461b2c45bdf2a7d7ac18363391de
Loussot <theo.loussot@gmail.com>
2023-01-25 16:50:46 +0100

    env hidden

--- sanity.ts
+++ sanity.ts
@@ -3,7 +3,6 @@ import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-    // projectId: 'b59u2nim',
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
     apiVersion:'2021-10-21',
     useCdn: process.env.NODE_END === "production",
