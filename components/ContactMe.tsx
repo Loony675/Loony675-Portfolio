a4bdf910458b2d7820001ccbb615eed82a1b2b28
Loussot <theo.loussot@gmail.com>
2023-03-22 21:32:08 +0100

    correctif style contact

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -19,18 +19,18 @@ export default function ContactMe({pageInfo}: Props) {
   };
   return (
     <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl space-y-10">
         Contact
       </h3>
       <div className=" flex flex-col space-y-10 ">
-        <h4 className="text-2xl font-semibold text-center">
+        {/* <h4 className="text-2xl font-semibold text-center">
           J'ai ce qu'il vous faut.
           <br />
           <span className="decoration-[#F7AB0A]/50 underline">
             Contactez-moi.
           </span>
-        </h4>
-        <div className="space-y-10 ml-5">
+        </h4> */}
+        <div className="space-y-10 ml-5 px-0">
           <div className="flex items-center space-x-5 ">
             <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
             <p className="text-2xl">{pageInfo.phoneNumber}</p>
@@ -46,7 +46,7 @@ export default function ContactMe({pageInfo}: Props) {
         </div>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col space-y-2 w-fit mx-auto"
+          className="flex flex-col space-y-2  "
         >
           <div className="flex space-x-2">
             <input
@@ -74,7 +74,7 @@ export default function ContactMe({pageInfo}: Props) {
             className="contactInput"
           />
           <button
-            className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
+            className="bg-[#F7AB0A] py-5 px-4  rounded-md text-black font-bold text-lg"
             type="submit"
           >
             Envoyer
